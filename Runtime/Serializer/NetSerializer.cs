a7eea3153384d453bfe16dd54e7aaab0013fefe9
Francisco Secchi Tarrés <fransecchifst@gmail.com>
2025-04-01 18:27:47 +0200

    Serializer - Abstracted

--- Runtime/Serializer/NetSerializer.cs
+++ Runtime/Serializer/NetSerializer.cs
@@ -2,16 +2,16 @@ namespace Serializer.NetPackage.Runtime.Serializer
 {
     public static class NetSerializer
     {
-        public static ISerialize _Serializer;
-        public static byte[] Serialize<T>(T data)
+        public static ISerialize _Serializer = new MPSerializer();
+
+        public static byte[] Serialize<T>(T data) where T : NetMessage
         {
             return _Serializer.Serialize(data);
         }
 
-        // Generic method to deserialize any object
-        public static T Deserialize<T>(byte[] data)
+        public static NetMessage Deserialize<T>(byte[] data) where T : NetMessage
         {
-            return _Serializer.Deserialize<T>(data);
+            return _Serializer.Deserialize<NetMessage>(data);
         }
     }
 }
--- Tests/Serializer/SerializerTests.cs
+++ Tests/Serializer/SerializerTests.cs
@@ -10,36 +10,37 @@ namespace SerializerTest.NetPackage.Tests.Serializer
     public class SerializerTests
     {
         private object objectTest;
+        private ISerialize _Serializer;
         [SetUp]
         public void Setup()
         {
-            NetSerializer._Serializer = new MPSerializer();
+            _Serializer = new MPSerializer();
         }
 
         [Test]
         public void StringTest()
         {
             objectTest = "Hello World";
-            byte[] pck = NetSerializer.Serialize(objectTest);
+            byte[] pck = _Serializer.Serialize(objectTest);
             
             Assert.IsNotNull(pck, "Serialized object is null");
-            Assert.IsTrue(objectTest.Equals(NetSerializer.Deserialize<object>(pck)), "Serialized object is incorrect");
+            Assert.IsTrue(objectTest.Equals(_Serializer.Deserialize<object>(pck)), "Serialized object is incorrect");
         }
         [Test]
         public void IntTest()
         {
             objectTest = 5678;
-            byte[] pck = NetSerializer.Serialize(objectTest);
+            byte[] pck = _Serializer.Serialize(objectTest);
             
             Assert.IsNotNull(pck, "Serialized object is null");
-            Assert.IsTrue(objectTest.Equals(NetSerializer.Deserialize<object>(pck)), "Serialized object is incorrect");
+            Assert.IsTrue(objectTest.Equals(_Serializer.Deserialize<object>(pck)), "Serialized object is incorrect");
         }
         [Test]
         public void ObjectTest()
         {
             MessageTest msg = new MessageTest(456, "Message");
-            byte[] pck = NetSerializer.Serialize(msg);
-            MessageTest msg2 = NetSerializer.Deserialize<MessageTest>(pck);
+            byte[] pck = _Serializer.Serialize(msg);
+            MessageTest msg2 = _Serializer.Deserialize<MessageTest>(pck);
             
             Assert.IsNotNull(pck, "Serialized object is null");
             Assert.IsTrue(msg.health.Equals(msg2.health) && msg.msg.Equals(msg2.msg), "Serialized object is incorrect");
@@ -49,8 +50,8 @@ namespace SerializerTest.NetPackage.Tests.Serializer
         {
             List<int> list = new List<int>();
             list.Add(5678);
-            byte[] pck = NetSerializer.Serialize(list);
-            List<int> list2 = NetSerializer.Deserialize<List<int>>(pck);
+            byte[] pck = _Serializer.Serialize(list);
+            List<int> list2 = _Serializer.Deserialize<List<int>>(pck);
             Assert.IsNotNull(pck, "Serialized object is null");
             Assert.IsNotNull(list2, "Deserialized list is null");
             Assert.IsTrue(list2.Count == list.Count, "Deserialized list count is incorrect");
@@ -62,8 +63,8 @@ namespace SerializerTest.NetPackage.Tests.Serializer
             List<MessageTest> list = new List<MessageTest>();
             list.Add(new MessageTest(5678, "Message")
             );
-            byte[] pck = NetSerializer.Serialize(list);
-            List<MessageTest> list2 = NetSerializer.Deserialize<List<MessageTest>>(pck);
+            byte[] pck = _Serializer.Serialize(list);
+            List<MessageTest> list2 = _Serializer.Deserialize<List<MessageTest>>(pck);
             
             Assert.IsNotNull(pck, "Serialized object is null");
             Assert.IsNotNull(list2, "Deserialized list is null");
@@ -76,8 +77,8 @@ namespace SerializerTest.NetPackage.Tests.Serializer
             List<MessageTest> list = new List<MessageTest>();
             list.Add(new MessageTest(5678, "Message", new List<int>(){0,1,2}));
             
-            byte[] pck = NetSerializer.Serialize(list);
-            List<MessageTest> list2 = NetSerializer.Deserialize<List<MessageTest>>(pck);
+            byte[] pck = _Serializer.Serialize(list);
+            List<MessageTest> list2 = _Serializer.Deserialize<List<MessageTest>>(pck);
             
             Assert.IsNotNull(pck, "Serialized object is null");
             Assert.IsNotNull(list2, "Deserialized list is null");
@@ -90,8 +91,8 @@ namespace SerializerTest.NetPackage.Tests.Serializer
             Dictionary<int,MessageTest> dic = new Dictionary<int,MessageTest>();
             dic[0] = new MessageTest(5678, "Message", new List<int>(){0,1,2});
             
-            byte[] pck = NetSerializer.Serialize(dic);
-            Dictionary<int,MessageTest> dic2 = NetSerializer.Deserialize< Dictionary<int,MessageTest>>(pck);
+            byte[] pck = _Serializer.Serialize(dic);
+            Dictionary<int,MessageTest> dic2 = _Serializer.Deserialize< Dictionary<int,MessageTest>>(pck);
             
             Assert.IsNotNull(pck, "Serialized object is null");
             Assert.IsNotNull(dic2, "Deserialized list is null");

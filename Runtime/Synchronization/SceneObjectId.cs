39667c3cde94c0b2b03c32591e287709a9505ab0
Francisco Secchi Tarrés <fransecchifst@gmail.com>
2025-05-19 21:28:47 +0200

    Scene Id consistent across applications

--- Runtime/Synchronization/SceneObjectId.cs
+++ Runtime/Synchronization/SceneObjectId.cs
@@ -6,27 +6,24 @@ namespace NetPackage.Synchronization
     [ExecuteAlways]
     public class SceneObjectId : MonoBehaviour
     {
-        [SerializeField]
-        private long _sceneId;
-        public long sceneId => _sceneId;
+        public long sceneId;
+
 
         private void Awake()
         {
-            if (_sceneId == 0)
-                _sceneId = BitConverter.ToInt64(Guid.NewGuid().ToByteArray(), 0);
+            if (sceneId == 0)
+            {
+                sceneId = BitConverter.ToInt64(Guid.NewGuid().ToByteArray(), 0);
+            }
         }
 
 #if UNITY_EDITOR
-        private void Reset()
-        {
-            if (_sceneId == 0)
-                _sceneId = BitConverter.ToInt64(Guid.NewGuid().ToByteArray(), 0);
-        }
-
         private void OnValidate()
         {
-            if (_sceneId == 0)
-                _sceneId = BitConverter.ToInt64(Guid.NewGuid().ToByteArray(), 0);
+            if (sceneId == 0)
+            {
+                sceneId = BitConverter.ToInt64(Guid.NewGuid().ToByteArray(), 0);
+            }
         }
 #endif
     }

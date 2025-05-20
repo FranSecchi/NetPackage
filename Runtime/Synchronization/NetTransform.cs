d1b2ae6633440c0c84a24a229652f16d92507a73
Francisco Secchi Tarrés <fransecchifst@gmail.com>
2025-05-20 16:21:11 +0200

    Update NetTransform.cs

--- Runtime/Synchronization/NetTransform.cs
+++ Runtime/Synchronization/NetTransform.cs
@@ -6,20 +6,26 @@ namespace NetPackage.Synchronization
     public class NetTransform : NetBehaviour
     {
         [Sync] private Vector3 _postition;
-        [Sync] private Quaternion _rotation;
+        [Sync] private float _rotationX;
+        [Sync] private float _rotationY;
+        [Sync] private float _rotationZ;
+        [Sync] private float _rotationW;
         [Sync] private Vector3 _scale;
 
         private void Update()
         {
             _postition = transform.position;
-            _rotation = transform.rotation;
+            _rotationX = transform.rotation.x;
+            _rotationY = transform.rotation.y;
+            _rotationZ = transform.rotation.z;
+            _rotationW = transform.rotation.w;
             _scale = transform.localScale;
         }
 
         private void LateUpdate()
         {
             transform.position = _postition;
-            transform.rotation = _rotation;
+            transform.rotation = new Quaternion(_rotationX, _rotationY, _rotationZ, _rotationW);
             transform.localScale = _scale;
         }
     }
